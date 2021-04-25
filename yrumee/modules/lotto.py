cae875399da93e50626e2679c51e13f9320c040f
Suho Lee <susemeee@gmail.com>
2021-04-25 23:58:41 +0900

    fix(LottoModule): syntax

--- yrumee/modules/lotto.py
+++ yrumee/modules/lotto.py
@@ -7,11 +7,11 @@ from yrumee.modules import Module
 
 class LottoModule(Module):
     """
-[.로또] 여름이가 추천하는 로또 번호를 출력합니다.
+    [.로또] 여름이가 추천하는 로또 번호를 출력합니다.
     """
 
     async def on_command(self, command: str, payload: str, message: discord.Message):
         if command == "로또":
-            lotto_num = sorted(random.sample(range(1, 46), 6)
+            lotto_num = sorted(random.sample(range(1, 46), 6))
             lotto_num = " ".join([str(x) for x in lotto_num])
             await message.channel.send("여름이의 로또 픽: {}".format(lotto_num))
